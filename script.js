707eee0c4d445172ed3016aeed4a2610646840da
kiran-0103vs <131228987+kiran-0103vs@users.noreply.github.com>
2024-08-22 20:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result = '';
+    const romanPairs = Object.values(obj);
+
+    // Loop through each symbol and value pair
+    for (let i = 0; i < romanPairs.length; i++) {
+        const [symbol, value] = romanPairs[i];
+        
+        // Handle subtractive notation
+        if (i % 2 === 0 && i < romanPairs.length - 2) {
+            const nextValue = romanPairs[i + 2][1];
+            const nextSymbol = romanPairs[i + 2][0];
+            
+            if (num >= value - nextValue) {
+                result += nextSymbol + symbol;
+                num -= value - nextValue;
+                continue;
+            }
+        }
+
+        // Add the symbol to the result as many times as it fits into num
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
