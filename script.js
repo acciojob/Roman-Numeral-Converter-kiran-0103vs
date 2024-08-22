f2f898bd13539878e4296eb9f53f2e506453a038
kiran-0103vs <131228987+kiran-0103vs@users.noreply.github.com>
2024-08-22 20:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 function convertToRoman(num) {
-  	const obj = {
-        0: ['M', 1000], 
-        1: ['D', 500], 
-        2: ['C', 100], 
-        3: ['L', 50], 
-        4: ['X', 10], 
-        5: ['V', 5], 
-        6: ['I', 1]
-    };
+  	const romanSymbols = [
+        ['M', 1000], 
+        ['CM', 900],
+        ['D', 500], 
+        ['CD', 400],
+        ['C', 100], 
+        ['XC', 90],
+        ['L', 50], 
+        ['XL', 40],
+        ['X', 10], 
+        ['IX', 9],
+        ['V', 5], 
+        ['IV', 4],
+        ['I', 1]
+    ];
 
     let result = '';
-    const romanPairs = Object.values(obj);
 
-    // Iterate over the Roman numeral symbols
-    for (let i = 0; i < romanPairs.length; i++) {
-        const [symbol, value] = romanPairs[i];
-
-        // Handle subtractive combinations (like IV, IX, etc.)
-        if (i % 2 === 0 && i < romanPairs.length - 2) {
-            const nextValue = romanPairs[i + 2][1];
-            const nextSymbol = romanPairs[i + 2][0];
-            
-            if (num >= value - nextValue) {
-                result += nextSymbol + symbol;
-                num -= value - nextValue;
-            }
-        }
-
-        // Append the Roman symbol while the value is smaller than or equal to num
+    // Loop through each symbol and value pair
+    for (let i = 0; i < romanSymbols.length; i++) {
+        const [symbol, value] = romanSymbols[i];
+        // Add the symbol to the result as many times as it fits into num
         while (num >= value) {
             result += symbol;
             num -= value;
@@ -35,7 +28,6 @@ function convertToRoman(num) {
     }
 
     return result;
-
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
